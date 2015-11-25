abc64410bf6d18898369599767fc68125b971521
raphael.zumbrunnen@students.fhnw.ch <raphael.zumbrunnen@students.fhnw.ch>
2015-11-25 04:16:49 +0100

    Weitere anpassungen an Projektstruktur

---
+++ src/main/java/ch/fhnw/oop/App.java
@@ -0,0 +1,13 @@
+package ch.fhnw.oop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
